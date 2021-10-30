224eaf5cc091603261f9d21ddeb0de31b259d5e9
ziansu <su284@purdue.edu>
2021-10-29 20:16:11 -0400

    add codebert-ft inference

--- notebooks/dep/justinfer/__init__.py
+++ notebooks/dep/justinfer/__init__.py
@@ -1,1 +1,9 @@
 # This is the module to faciliate inference
+from . import codebert
+
+def process(model_type, output_dir, load_model_path, test_filename, 
+    batch_size):
+
+    if model_type == 'codebert-ft':
+        codebert.infer(output_dir, load_model_path, 
+            test_filename, batch_size)
